3b3616535598391235127266a2be4e503a4c003b
Ann <pppimka@gmail.com>
2015-05-26 16:50:29 +0300

    converter class

---
+++ src/main/java/com/bionic/restaurantTry1/utils/LocalDateTimePersistenceConverter.java
@@ -0,0 +1,23 @@
+package com.bionic.restaurantTry1.utils;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class LocalDateTimePersistenceConverter implements
+		AttributeConverter<LocalDateTime, Timestamp> {
+
+	@Override
+	public java.sql.Timestamp convertToDatabaseColumn(LocalDateTime entityValue) {
+		return Timestamp.valueOf(entityValue);
+	}
+
+	@Override
+	public LocalDateTime convertToEntityAttribute(
+			java.sql.Timestamp databaseValue) {
+		return databaseValue.toLocalDateTime();
+	}
+}
