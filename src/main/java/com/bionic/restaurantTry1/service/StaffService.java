10f62e4ad786c4368902b55eedf6cf400f9b2193
pppimka <pppimka@gmail.com>
2015-08-14 01:13:17 +0300

    see

--- src/main/java/com/bionic/restaurantTry1/service/StaffService.java
+++
@@ -1,9 +0,0 @@
-package com.bionic.restaurantTry1.service;
-
-import com.bionic.restaurantTry1.entities.Staff;
-
-public interface StaffService {
-	public Staff findById(int id);
-	
-	public Staff findByLogin(String login);
-}
